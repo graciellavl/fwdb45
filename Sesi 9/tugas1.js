39a8e907995f813aff7ca14be9bb7246cec58eb3
graciellavl <graciellavl@users.noreply.github.com>
2021-03-18 10:45:59 +0700

    add final project

---
+++ Sesi 9/tugas1.js
@@ -0,0 +1,15 @@
+// Graciella Valeska Liander
+
+var recipeIndomieSeblakMakaroni = [
+    "1. Tumis bumbu halus dengan sedikit minyak",
+    "2. masukkan air, setelah mendidih masukkan kocokan telur diaduk",
+    "3. masukkan makaroni, lalu sosis kerupuk dan mi, beri gula garam bumbu indomi",
+    "4. aduk rata tes rasa angkat sajikan taburi dengan daun bawang.",
+];
+
+// Mengakses elemen pertama
+console.log(recipeIndomieSeblakMakaroni[0]);
+
+// Mengakses elemen terakhir
+var lastStep = recipeIndomieSeblakMakaroni[3];
+console.log(lastStep);
---
+++ Sesi 9/tugas2.js
@@ -0,0 +1,29 @@
+// Graciella Valeska Liander
+
+var myBucketList = ["Menjadi front end developer", "Memiliki web sendiri"];
+
+console.log("Array awal:");
+console.log(myBucketList);
+
+// Menambah item ke paling belakang array
+console.log("Push item:");
+myBucketList.push("Mendapatkan beasiswa GIT");
+console.log(myBucketList);
+
+// Menambah item ke paling depan array
+console.log("Unshift item:");
+myBucketList.unshift("Mendaftar beasiswa GIT");
+console.log(myBucketList);
+
+// Menghapus item paling belakang array
+console.log("Pop item:");
+myBucketList.pop();
+console.log(myBucketList);
+
+// Menghapus item paling depan array
+console.log("Shift item:");
+myBucketList.shift();
+console.log(myBucketList);
+
+console.log("Array akhir:");
+console.log(myBucketList);
